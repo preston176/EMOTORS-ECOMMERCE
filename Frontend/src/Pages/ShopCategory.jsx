275c3bb1b80a178c0e788b796b9a1e54d65eaa37
preston176 <prestonnyamweya@gmail.com>
2024-04-01 21:16:33 +0300

    Added "Sort By" functionality

--- Frontend/src/Pages/ShopCategory.jsx
+++ Frontend/src/Pages/ShopCategory.jsx
@@ -1,73 +1,96 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './CSS/ShopCategory.css'
-
+import React, { useContext, useEffect, useState } from 'react';
+import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
 import Item from '../Components/Item/Item';
 
-
 const ShopCategory = (props) => {
+  const [bikes, setBikes] = useState([]);
+  const [displayedItems, setDisplayedItems] = useState(6); // Initial number of items displayed
+  const [sortBy, setSortBy] = useState('default'); // Default sorting
 
   useEffect(() => {
-    fetch('http://localhost:3000/products',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+    fetch('http://localhost:3000/products', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
       .then(response => response.json())
       .then(data => {
         setBikes(data);
-        console.log(data)
+        console.log(data);
       })
       .catch(error => {
         console.error('Error:', error);
       });
-
-
   }, []);
 
-  // const { all_product } = useContext(ShopContext)
-  const [bikes, setBikes] = useState([]);
+  // Function to handle "Explore More" button click event
+  const handleExploreMore = () => {
+    setDisplayedItems(prevCount => prevCount + 6); // Increase by 6 items each time
+  };
 
+  // Function to handle sorting
+  const handleSortBy = (value) => {
+    setSortBy(value);
+    let sortedBikes = [...bikes];
 
+    switch (value) {
+      case 'price':
+        sortedBikes.sort((a, b) => a.price - b.price);
+        break;
+      case 'name':
+        sortedBikes.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      // Add more cases for other sorting options as needed
+      default:
+        // Default sorting logic (if any)
+        break;
+    }
 
+    setBikes(sortedBikes);
+  };
 
 
   return (
     <div className='shop-category'>
-      <img className='shopcategory-banner' src={props.banner} alt="banner image" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Showing {props.category} bikes </span>
         </p>
         <div className="shopcategory-sort">
-          Sort by <img src={dropdown_icon} alt='' />
+          Sort by
+          <select className='sort-select' onChange={(e) => handleSortBy(e.target.value)}>
+            <option value="name">Name</option>
+            <option value="price">Price</option>
+          
+          </select>
+          {/* <img src={dropdown_icon} alt='' /> */}
         </div>
       </div>
       <div className="shopcategory-products">
-        {bikes.map((item, index) => {
-          if (props.category === item.category) {
-            return (
-              <Item
-                key={crypto.randomUUID()}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.price}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {bikes
+          .filter(item => props.category === item.category)
+          .slice(0, displayedItems)
+          .map((item, index) => (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              category={item.category}
+              new_price={item.price}
+            />
+          ))}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>
+      {displayedItems < bikes.length && (
+        <div className="shopcategory-loadmore" onClick={handleExploreMore}>
+          Explore More
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
